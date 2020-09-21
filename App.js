ce68f1f76e63eef30bef11e959cfc4c7dfb021c3
dx00 <scripter00@outlook.com>
2020-09-20 21:37:13 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,114 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { StyleSheet, View, Button, BackHandler } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
+
+const Stack = createStackNavigator();
+
+
+
+// Criei aqui 2 componentes screens
+function Home({ navigation }){
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      
+      <Button title="Info" onPress={ () => { navigation.navigate('Info') } } />
+
     </View>
   );
-}
+};
+
+
+function Info() {
+
+  return (
+    <View style={ styles.container }>
+
+      <WebviewUi /> 
+
+    </View>
+  );
+};
+
+// criei aqui um componente webview personalizado para a navegação nativa do android
+function WebviewUi(){
+
+
+  const webref = useRef(null);
+  const navigation = useNavigation();
+
+
+  function webViewGoBack(props) 
+  {
+
+      if( props.canGoBack )
+      {
+
+          BackHandler.addEventListener("hardwareBackPress", () => {
+              webref.current.goBack();
+              return true;
+          });
+
+      }
+
+      else if( !props.canGoBack ) 
+      {
+
+          BackHandler.addEventListener("hardwareBackPress", () => {
+
+            navigation.navigate("Home");
+
+            BackHandler.addEventListener("hardwareBackPress", () => {
+                BackHandler.exitApp();
+            });
+
+            return true;
+        
+          });           
+      };
+
+  };
+
+
+
+  return (
+      
+      <WebView 
+        onNavigationStateChange={webViewGoBack}
+        source={{ uri: 'https://reactnative.dev/' }} 
+        ref={webref}
+      />
+
+    );
+};
+
+
+
+
+
+
+export default function App() {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Info" component={Info} />
+ 
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f9f9f9',
   },
 });
